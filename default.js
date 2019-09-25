d3328d0ab04447b69de53a2e539c1f71ed22b6c6
0x41mmarVM <48731797+0x41mmarVM@users.noreply.github.com>
2019-09-26 00:56:17 +0100

    Create default.js

---
+++ default.js
@@ -0,0 +1,22 @@
+// sample script
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+/**
+ * Fetch and return the request body
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  try {
+    /* Modify request here before sending it with fetch */
+    console.log(request.url);
+    let response = await fetch(request);
+
+    /* Modify response here before returning it */
+
+    return response;
+  } catch (e) {
+    return new Response(e.stack || e, { status: 500 });
+  }
+}
